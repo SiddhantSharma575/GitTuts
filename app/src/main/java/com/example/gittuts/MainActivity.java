88af708139598ad2f1d2796e9e5cb8cff8d0523d
Siddhant <siddhantsharma909@gmail.com>
2021-07-10 07:52:37 +0530

    This is Our Third Commit

--- app/src/main/java/com/example/gittuts/MainActivity.java
+++ app/src/main/java/com/example/gittuts/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("This is Our Second Commit");
+        today is a good day
 
     }
 }
